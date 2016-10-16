40b6134ac79a1272ff44f48bdee6bd2c515c393e
fwetekamp <fabian.we@gmail.com>
2016-10-16 12:48:40 +0200

    Initial Commit

---
+++ Persistence_String/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Persistence_String
+//
+//  Created by Fabian Wetekamp on 16/10/2016.
+//  Copyright © 2016 Fabian Wetekamp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
