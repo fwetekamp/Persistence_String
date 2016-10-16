39fb10682731a59315ef7f9a54ee91cc06ac377e
fwetekamp <fabian.we@gmail.com>
2016-10-16 12:56:13 +0200

    Sample project that shows simple data persistence for strings
    Swift 3/ iOS10

--- Persistence_String/ViewController.swift
+++ Persistence_String/ViewController.swift
@@ -1,25 +1,59 @@
 //
 //  ViewController.swift
-//  Persistence_String
+//  testingpersistence2
 //
-//  Created by Fabian Wetekamp on 16/10/2016.
+//  Created by Fabian Wetekamp on 15/10/2016.
 //  Copyright © 2016 Fabian Wetekamp. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var users = [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let savedUsers = loadUsers(){
+            users += savedUsers
+        }else{
+            loadSampleUsers()
+        }
+        lastsaveduser.text = users.last?.name
     }
-
+    
+    func  loadSampleUsers(){
+        let user1 = User(name: "Hannes")
+        users = [user1!]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBOutlet weak var lastsaveduser: UILabel!
+    @IBOutlet weak var enternewuser: UITextField!
+    
+    @IBAction func saveuser(_ sender: AnyObject) {
+        let newvalue = enternewuser.text
+        lastsaveduser.text = newvalue
+        users.append(User(name: newvalue!)!)
+        saveUsers()
+    }
+    
+    
+    func saveUsers(){
+        let isSuccessfullSave = NSKeyedArchiver.archiveRootObject(users, toFile: User.ArchiveURL.path)
+        if !isSuccessfullSave{
+            print("Failed to save users")
+        }
+    }
+    
+    func  loadUsers() -> [User]?{
+        return NSKeyedUnarchiver.unarchiveObject(withFile: User.ArchiveURL.path) as? [User]
+    }
+    
+    
 }
-
